8230e1b744613db4a710fd1c28f808a2c81576c5
nikosMos <moschos30@hotmail.com>
2026-05-28 16:58:02 +0300

    feat(backend): add buffering application startup for tracking startup metrics

--- backend/src/main/java/com/example/ExpenseTracker/ExpenseTrackerApplication.java
+++ backend/src/main/java/com/example/ExpenseTracker/ExpenseTrackerApplication.java
@@ -1,12 +1,10 @@
 package com.example.ExpenseTracker;
-
-import com.example.ExpenseTracker.config.rateLimitingConfig.RateLimitingProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
@@ -14,7 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args)  {
-		SpringApplication.run
-                (ExpenseTrackerApplication.class, args);
+		SpringApplication app = new SpringApplication(ExpenseTrackerApplication.class);
+        app.setApplicationStartup(new BufferingApplicationStartup(2048));
+
+        app.run(args);
 	}
 }

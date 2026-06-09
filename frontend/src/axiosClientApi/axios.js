e3d54ff7e395c453bb1dfb49ba6eb670beafd10f
nikosMos <moschos30@hotmail.com>
2026-06-09 09:37:37 +0300

    fix(auth): implement 401 interceptor for jwt expiration

--- frontend/src/axiosClientApi/axios.js
+++ frontend/src/axiosClientApi/axios.js
@@ -5,4 +5,21 @@ const api = axios.create({
     withCredentials: true,
 });
 
+api.interceptors.response.use(
+    (response) => {
+        return response.data;
+    },
+    (error) => {
+        if (error.response?.status === 401) {
+            queryClient.removeQueries({ queryKey: ["verification"] });
+
+            localStorage.removeItem("REACT_QUERY_OFFLINE_CACHE");
+
+            window.location.replace("/login");
+        }
+
+        return Promise.reject(error);
+    }
+)
+
 export default api;

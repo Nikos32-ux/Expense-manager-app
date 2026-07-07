99ae953ce08b4d9875f76cd16a07e87e911a553a
nikosMos <moschos30@hotmail.com>
2026-07-07 17:15:48 +0300

    test(report): update Report Service tests

--- backend/src/test/java/com/example/ExpenseTracker/service/Report/ReportServiceImplTest.java
+++ backend/src/test/java/com/example/ExpenseTracker/service/Report/ReportServiceImplTest.java
@@ -1,15 +1,16 @@
 package com.example.ExpenseTracker.service.Report;
-import com.example.ExpenseTracker.dto.CheckReportExistsDTO;
-import com.example.ExpenseTracker.dto.ReportActionResponse;
-import com.example.ExpenseTracker.dto.SendReportWebSocketDTO;
+import com.example.ExpenseTracker.dto.*;
 import com.example.ExpenseTracker.exception.ReportNotFoundException;
 import com.example.ExpenseTracker.model.Expense;
 import com.example.ExpenseTracker.model.Notification;
 import com.example.ExpenseTracker.model.Report;
 import com.example.ExpenseTracker.model.User;
+import com.example.ExpenseTracker.repository.ExpenseRepository;
 import com.example.ExpenseTracker.repository.NotificationRepository;
 import com.example.ExpenseTracker.repository.ReportRepository;
 import com.example.ExpenseTracker.repository.UserRepository;
+import com.example.ExpenseTracker.service.CloudinaryService;
+import com.example.ExpenseTracker.service.report.ReportMessageProducer;
 import com.example.ExpenseTracker.service.report.ReportServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -25,7 +26,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -43,10 +46,22 @@ public class ReportServiceImplTest {
     private UserRepository userRepository;
 
     @Mock
+    private ReportMessageProducer reportMessageProducer;
+
+    @Mock
+    CloudinaryService cloudinaryService;
+
+    @Mock
+    ExpenseRepository expenseRepository;
+
+    @Mock
     private NotificationRepository notificationRepository;
 
     @Mock
     private SimpMessagingTemplate simpMessagingTemplate;
+
+
+    private ReportActionResponse reportActionResponse;
 
     @BeforeEach
     void setup(){
@@ -145,22 +160,81 @@ public class ReportServiceImplTest {
     }
 
     @Nested
+    class createIfNeeded{
+        Long userId = 10L;
+        String email = "test@gmail.com";
+
+        ReportActionResponse staleResponse = new ReportActionResponse(
+                "STALE",
+                "old-path",
+                LocalDateTime.now()
+        );
+
+        @Test
+        void whenReportIsNull_shouldInsertAndSendMessage(){
+            when(reportRepository.insertReport(userId))
+                    .thenReturn(1);
+
+            reportService.createIfNeeded(null, userId, email);
+
+            verify(reportRepository).insertReport(userId);
+            verify(reportMessageProducer).sendReportTask(userId, email);
+            verify(reportRepository, never()).setReportStatus(userId);
+        }
+
+        @Test
+        void whenReportExistsAndStatusStale_shouldUpdateAndSendMessage(){
+            when(reportRepository.setReportStatus(userId))
+                    .thenReturn(1);
+
+            reportService.createIfNeeded(staleResponse, userId, email);
+
+            verify(reportRepository).setReportStatus(userId);
+            verify(reportMessageProducer).sendReportTask(userId, email);
+
+            verify(reportRepository, never()).insertReport(anyLong());
+        }
+
+        @Test
+        void whenInsertFails_shouldNotSendMessage(){
+            when(reportRepository.insertReport(userId))
+                    .thenReturn(0);
+
+            reportService.createIfNeeded(null, userId, email);
+
+            verify(reportRepository).insertReport(userId);
+            verify(reportMessageProducer, never())
+                    .sendReportTask(userId, email);
+            verify(reportRepository, never()).setReportStatus(anyLong());
+
+        }
+        @Test
+        void whenUpdateFails_shouldNotSendMessage(){
+            when(reportRepository.setReportStatus(userId))
+                    .thenReturn(0);
+
+            reportService.createIfNeeded(staleResponse, userId, email);
+
+            verify(reportRepository).setReportStatus(anyLong());
+            verify(reportRepository, never()).insertReport(userId);
+            verify(reportMessageProducer, never())
+                    .sendReportTask(userId, email);
+        }
+    }
+
+    @Nested
     class updateOrCreate{
         @Test
-        void updateOrCreate_ReportExists_success(){
-            Long userId = 1L;
-
+        void shouldUpdateReportAndAddNotification(){
             User user = new User();
-            user.setId(userId);
+            user.setId(1L);
             String email = "test@test.com";
             String file_url = "file-123";
-
-            Report report = new Report();
-            report.setId(100L);
-
+            Report existingReport = new Report();
+            existingReport.setId(100L);
 
             when(reportRepository.findByUserId(user.getId()))
-                    .thenReturn(Optional.of(report));
+                    .thenReturn(Optional.of(existingReport));
 
             reportService.updateOrCreate(user, file_url, email);
 
@@ -193,7 +267,7 @@ public class ReportServiceImplTest {
         }
 
         @Test
-        void updateOrCreate_ReportNotExist_shouldThrow(){
+        void shouldThrowReportNotFound_ReportNotExist(){
             Long userId = 1L;
             User user = new User();
             user.setId(userId);
@@ -206,6 +280,69 @@ public class ReportServiceImplTest {
             assertThrows(ReportNotFoundException.class, () -> {
                 reportService.updateOrCreate(user, file_url, email);
             });
+        }
+    }
+
+    @Nested
+    class createReport{
+        Long userId = 1L;
+
+        @Test
+        void shouldReturnEarlyAndSkipProcessing_reportExists(){
+            Long userId = 1L;
+            String email = "test@gmail.com";
+
+            TaskMessageDTO taskMessageDTO = new TaskMessageDTO(userId, email);
+
+            ReportStatusProjection existingReportStatus = mock(ReportStatusProjection.class);
+            when(existingReportStatus.getStatus()).thenReturn("DONE");
+
+            when(reportRepository.findStatusByUserId(userId))
+                    .thenReturn(Optional.of(existingReportStatus));
+
+            reportService.createReport(taskMessageDTO);
+
+            verify(reportRepository, never()).findByUserId(userId);
+            verify(reportRepository).findStatusByUserId(userId);
+            verifyNoInteractions(userRepository);
+            verifyNoInteractions(expenseRepository);
+            verifyNoInteractions(cloudinaryService);
+            verifyNoInteractions(notificationRepository);
+            verifyNoInteractions(simpMessagingTemplate);
+        }
+
+        @Test
+        void shouldCompleteProcessing_reportNotExist(){
+            User mockUser = new User();
+            mockUser.setId(userId);
+            Report report = new Report();
+            report.setStatus("PROCESSING");
+
+
+            ExpenseReportDTO mockData = mock(ExpenseReportDTO.class);
+
+            List<ExpenseReportDTO> dataList = List.of(mockData);
+            when(reportRepository.findStatusByUserId(userId))
+                    .thenReturn(Optional.empty());
+            when(expenseRepository.findReportByUserId(userId))
+                    .thenReturn(dataList);
+            when(userRepository.getReferenceById(userId))
+                    .thenReturn(mockUser);
+            when(reportRepository.findByUserId(userId))
+                    .thenReturn(Optional.of(report));
+            when(cloudinaryService.updloadCsvFile(any(), eq(mockUser.getId())))
+                    .thenReturn("file-123");
+
+            TaskMessageDTO task = new TaskMessageDTO(userId, "test@test.com");
+
+            reportService.createReport(task);
+
+            verify(reportRepository).findStatusByUserId(userId);
+            verify(userRepository).getReferenceById(userId);
+            verify(expenseRepository).findReportByUserId(userId);
+            verify(cloudinaryService).updloadCsvFile(any(), eq(mockUser.getId()));
+            verify(reportRepository).save(any(Report.class));
+            verify(notificationRepository).save(any(Notification.class));
         }
     }
 }

c2a62bd2db4e152406c469643d7ab2870cb22e38
nikosMos <moschos30@hotmail.com>
2026-07-20 13:52:32 +0300

    test(repository): add DataJpaTest for expenseRepository

--- backend/src/main/java/com/example/ExpenseTracker/repository/ExpenseCatRepository.java
+++ backend/src/main/java/com/example/ExpenseTracker/repository/ExpenseCatRepository.java
@@ -4,7 +4,9 @@ import com.example.ExpenseTracker.model.ExpenseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExpenseCatRepository extends JpaRepository<ExpenseCategory, Long> {
-
+     Optional<ExpenseCategory> findByCategory(String category);
 }

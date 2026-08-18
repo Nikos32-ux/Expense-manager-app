56c2a5a766160ca6f82c6b405cceaf1418fa2055
nikosMos <moschos30@hotmail.com>
2026-08-18 17:49:04 +0300

    docs(expense-controller): add api documentation for expensecontroller endpoints

--- backend/src/main/java/com/example/ExpenseTracker/controller/ExpenseController.java
+++ backend/src/main/java/com/example/ExpenseTracker/controller/ExpenseController.java
@@ -5,6 +5,10 @@ import com.example.ExpenseTracker.model.Expense;
 import com.example.ExpenseTracker.model.ExpenseCategory;
 import com.example.ExpenseTracker.service.Expense.ExpenseService;
 import com.example.ExpenseTracker.util.UserContextUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,7 +24,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -30,10 +33,19 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 @RequestMapping("/expenses")
 @Validated
+@Tag(name = "Expenses", description = "Expense management endpoints")
 public class ExpenseController {
 
     private final ExpenseService expenseService;
 
+    @Operation(
+            summary = "Get dashboard page expenses",
+            description = "Retrieves a Page containing the five most recent expenses for display on the client dashboard.Returns an empty Page when the user has no expenses"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Dashboard expenses successfully retrieved"
+    )
     @GetMapping("get-dashboard-expenses")
     public ResponseEntity<Page<ExpenseResDTO>> dashboardExpenses(){
         Long userId = UserContextUtils.getAuthenticatedUser().getId();
@@ -45,7 +57,14 @@ public class ExpenseController {
         return ResponseEntity.ok().body(topExpenses);
     };
 
-
+    @Operation(
+            summary = "Get expenses for transactions page",
+            description = "Retrieves paginated expenses for display on the client transactions page. By default, the five most recent expenses are returned"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Transactions page expenses successfully retrieved"
+    )
     @GetMapping("get-expenses")
     public ResponseEntity<Page<ExpenseResDTO>> getExpenses(
             @Valid @ModelAttribute ExpensesFilters filters,
@@ -66,13 +85,35 @@ public class ExpenseController {
         return ResponseEntity.ok(expenses);
     }
 
+    @Operation(
+            summary = "Get expense by expense id",
+            description = "Retrieves  expense by id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Expense successfully retrieved"
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Expense not found"
+    )
     @GetMapping("get-expense/{id}")
-    public ResponseEntity<ExpenseResDTO> getExpense(@PathVariable Long id){
+    public ResponseEntity<ExpenseResDTO> getExpense(
+            @Parameter(description = "Id of expense to retrieve", example = "1")
+            @PathVariable Long id){
         Long userId = UserContextUtils.getAuthenticatedUser().getId();
         ExpenseResDTO expense = expenseService.getExpenseById(id, userId);
         return ResponseEntity.status(HttpStatus.OK).body(expense);
     }
 
+    @Operation(
+            summary = "Get expenses monthly total ",
+            description = "Retrieves the current month's total expenses.If no expenses exist, it returns the current month with a zero total"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Month total data successfully retrieved"
+    )
     @GetMapping("expense-month-total")
     public ResponseEntity<MonthExpensesTotalInterface> getTotalSum(){
         Long userId = UserContextUtils.getAuthenticatedUser().getId();
@@ -80,7 +121,14 @@ public class ExpenseController {
         return ResponseEntity.status(HttpStatus.OK).body(monthTotal);
     }
 
-
+    @Operation(
+            summary = "Get expense totals for all categories by filter",
+            description = "Retrieves a breakdown of total expenses per category and filtered by a day/week/month filter."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Categories totals successfully retrieved"
+    )
     @GetMapping("categories-total")
     public ResponseEntity<List<CategoryTotalDTO>> categoriesTotal(
             @Valid
@@ -92,6 +140,14 @@ public class ExpenseController {
     }
 
 
+    @Operation(
+            summary = "Get expense categories",
+            description = "Retrieves the list of expense categories."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Categories list successfully retrieved."
+    )
     @GetMapping("get-categories")
     public ResponseEntity<List<ExpenseCategory>> getCategories(){
         List<ExpenseCategory> categoriesList = expenseService.getExpenseCategories();
@@ -100,9 +156,18 @@ public class ExpenseController {
                 .body(categoriesList);
     }
 
+    @Operation(
+            summary = "Add expense",
+            description = "Creates a new expense for the user using an idempotency key to prevent concurrent submissions."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Expense is successfully created"
+    )
     @PostMapping("add_expense")
     public ResponseEntity<AddExpenseResDTO> addExpense(
             @Valid @RequestBody ExpenseReqDTO expenseReqDTO,
+            @Parameter(description = "Unique key to prevent duplicate submissions", example = "unique-key-12343")
             @RequestHeader("Idempotency-Key")
             @NotBlank(message = "{idempotency.key.required}")
             @Size(min = 10, max = 50, message = "{idempotency.key.invalid.size}")
@@ -114,16 +179,38 @@ public class ExpenseController {
                 .body(createExpenseStatus);
     }
 
+    @Operation(
+            summary = "Update expense",
+            description = "Updates an expense by id."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Expense is successfully updated."
+    )
     @PutMapping("update_expense/{expenseId}")
-    public ResponseEntity<ExpenseResDTO> updateExpense(@PathVariable Long expenseId, @Valid @RequestBody ExpenseReqDTO expenseReqDTO ){
+    public ResponseEntity<ExpenseResDTO> updateExpense(
+            @Parameter(description = "Id of existing expense", example = "1")
+            @PathVariable Long expenseId,
+            @Valid @RequestBody ExpenseReqDTO expenseReqDTO ){
         Long userId = UserContextUtils.getAuthenticatedUser().getId();
         ExpenseResDTO updated = expenseService.updateExpense(expenseId, expenseReqDTO, userId);
        return ResponseEntity.ok(updated);
     }
 
 
+
+    @Operation(
+            summary = "Delete an expense",
+            description = "Perform soft delete for an expense by id."
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "Expense is successfully deleted."
+    )
     @PutMapping("delete-expense/{expenseId}")
-    public ResponseEntity<String> deleteExpense(@PathVariable Long expenseId){
+    public ResponseEntity<String> deleteExpense(
+            @Parameter(description = "Id of existing expense", example = "1")
+            @PathVariable Long expenseId){
         Long userId = UserContextUtils.getAuthenticatedUser().getId();
         expenseService.deleteExpense(expenseId, userId);
         return ResponseEntity.noContent().build();
--- backend/src/main/java/com/example/ExpenseTracker/dto/ExpensesFilters.java
+++ backend/src/main/java/com/example/ExpenseTracker/dto/ExpensesFilters.java
@@ -1,5 +1,6 @@
 package com.example.ExpenseTracker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
@@ -9,15 +10,31 @@ public record ExpensesFilters (
               regexp = "(?i)^(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)$",
               message = "{validation.filters.month.invalid}"
       )
+      @Schema(
+              description = "Filter expenses by month name",
+              example = "AUGUST"
+      )
       String month,
       String category,
 
       @PositiveOrZero(message = "{validation.filters.amountMin.positive}")
+      @Schema(
+              description = "Minimum expense amount filter",
+              example = "10"
+      )
       Integer amountMin,
 
       @PositiveOrZero(message = "{validation.filters.amountMax.positive}")
+      @Schema(
+              description = "Maximum expense amount filter",
+              example = "200"
+      )
       Integer amountMax,
 
       @Size(max = 50, message = "{validation.filters.search.maxSize}")
+      @Schema(
+              description = "Search expenses by matching expense text description",
+              example = "Groceries from farmers market"
+      )
       String search
     ){}

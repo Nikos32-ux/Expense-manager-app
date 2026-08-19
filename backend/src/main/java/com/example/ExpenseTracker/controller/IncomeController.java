eeabdafea4484c66c5001e3e913a80ccc9fe8c97
nikosMos <moschos30@hotmail.com>
2026-08-19 12:09:06 +0300

    docs(income-controller): add OpenAPI documentation to IncomeController endpoints

--- backend/src/main/java/com/example/ExpenseTracker/controller/IncomeController.java
+++ backend/src/main/java/com/example/ExpenseTracker/controller/IncomeController.java
@@ -6,6 +6,10 @@ import com.example.ExpenseTracker.dto.MonthIncomeTotalInterface;
 import com.example.ExpenseTracker.model.IncomeCategory;
 import com.example.ExpenseTracker.service.income.IncomeService;
 import com.example.ExpenseTracker.util.UserContextUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,16 +27,33 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/income")
 @Validated
+@Tag(name = "Income", description = "Income management endpoints")
 public class IncomeController {
 
     private final IncomeService incomeService;
 
+    @Operation(
+            summary = "Get incomes.",
+            description = "Retrieves incomes for user's report.When no incomes exist it returns empty list."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Incomes successfully retrieved"
+    )
     @GetMapping("/get-all-incomes")
     public ResponseEntity<List<IncomeDataDTO>> getAllIncomes(Authentication auth){
        List<IncomeDataDTO> list = incomeService.getIncomes(auth);
        return ResponseEntity.ok().body(list);
     }
 
+    @Operation(
+            summary = "Get incomes monthly total ",
+            description = "Retrieves the current month's total incomes.If no expenses exist, it returns the current month with a zero total"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Month income total data successfully retrieved"
+    )
     @GetMapping("income-month-total")
     public ResponseEntity<MonthIncomeTotalInterface> getTotalMonthIncome(){
         Long userId = UserContextUtils.getAuthenticatedUser().getId();
@@ -40,22 +61,53 @@ public class IncomeController {
         return ResponseEntity.ok().body(list);
     }
 
+    @Operation(
+            summary = "Get income categories.",
+            description = "Retrieves income categories."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Income categories successfully retrieved"
+    )
     @GetMapping("/get-income-categories")
     public ResponseEntity<List<IncomeCategory>> getIncomeCategories(){
         List<IncomeCategory> list = incomeService.getCategories();
         return ResponseEntity.ok().body(list);
     }
 
+    @Operation(
+            summary = "Get income by id.",
+            description = "Retrieves specific income filtered by its id."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Income by id successfully retrieved"
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Income was not found."
+    )
     @GetMapping("/get-income/{incomeId}")
-    public ResponseEntity<IncomeDataDTO> getIncome(@PathVariable Long incomeId, Authentication auth){
+    public ResponseEntity<IncomeDataDTO> getIncome(
+            @Parameter(description = "The id of a specific income to be updated.", example = "1")
+            @PathVariable Long incomeId, Authentication auth){
       IncomeDataDTO requestedIncome = incomeService.getIncome(incomeId, auth);
       return ResponseEntity.ok().body(requestedIncome);
     }
 
+    @Operation(
+            summary = "Add income.",
+            description = "Creates a new income record for the user using an idempotency key to prevent concurrent submissions."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Income record is successfully created."
+    )
     @PostMapping("/add-income")
     public ResponseEntity<IncomeResDTO> addIncome(
             @Valid @RequestBody AddIncomeRequestDTO addIncomeReqDTO,
             Authentication auth,
+            @Parameter(description = "Unique key to prevent duplicate submissions", example = "unique-key-12343")
             @NotBlank(message = "{idempotency.key.required}")
             @Size(min = 10, max = 50, message = "{idempotency.key.invalid.size}")
             @RequestHeader("Idempotency-key") String idempotencyKey
--- backend/src/main/java/com/example/ExpenseTracker/dto/AddIncomeRequestDTO.java
+++ backend/src/main/java/com/example/ExpenseTracker/dto/AddIncomeRequestDTO.java
@@ -1,22 +1,35 @@
 package com.example.ExpenseTracker.dto;
-
-
 import com.example.ExpenseTracker.model.IncomeCategory;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 
 public record AddIncomeRequestDTO(
         @NotNull(message = "{income.amount.notNull}")
         @Positive(message = "{income.amount.positive}")
         @Digits(integer = 12, fraction = 2)
+        @Schema(
+                description = "Income Amount",
+                example = "500.99"
+        )
         BigDecimal amount,
         @NotNull(message = "{income.source.notNull}")
+        @Schema(
+                description = "Income source",
+                example = "SALARY"
+        )
         IncomeCategory source,
+
+
         @NotNull(message = "{income.date.notNull}")
         @PastOrPresent(message = "{income.date.time}")
+        @Schema(
+                description = "Income creation date.",
+                example = "2026-08-08"
+        )
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate date
 ) {}

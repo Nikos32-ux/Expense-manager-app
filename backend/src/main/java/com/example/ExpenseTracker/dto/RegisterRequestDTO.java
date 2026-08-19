75276a3f89e301b4a100a8a4f58336f56193fba1
nikosMos <moschos30@hotmail.com>
2026-08-19 11:10:30 +0300

    docs(auth-controller): add OpenAPI documentation for authcontroller endpoints

--- backend/src/main/java/com/example/ExpenseTracker/dto/RegisterRequestDTO.java
+++ backend/src/main/java/com/example/ExpenseTracker/dto/RegisterRequestDTO.java
@@ -1,18 +1,24 @@
 package com.example.ExpenseTracker.dto;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
 public record RegisterRequestDTO(
         @NotBlank(message="{register.username.notBlank}")
         @Size(min=6, max=20, message="{register.username.size}")
+        @Schema(
+                description = "Username for the account.",
+                example = "TestDev"
+        )
         String username,
 
         @NotBlank(message ="{register.email.notBlank}" )
         @Email(message = "{register.email.properFormat}")
+        @Schema(
+                description = "Email.",
+                example = "test@gmail.com"
+        )
         String email,
 
         @Pattern(
@@ -20,7 +26,17 @@ public record RegisterRequestDTO(
                 message = "{register.password.pattern}"
         )
         @NotBlank(message="{register.password.notBlank}")
+        @Schema(
+                description = "Password for the account. Must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+                example = "TestDev1!"
+        )
         String password,
 
+        @NotNull(message = "{register.imageProfile.notNull}")
+        @Schema(
+                description = "Profile image to upload",
+                type = "string",
+                format = "binary"
+        )
         MultipartFile imageProfile
 ) {}
--- backend/src/main/java/com/example/ExpenseTracker/dto/UpdateAccountReqDTO.java
+++ backend/src/main/java/com/example/ExpenseTracker/dto/UpdateAccountReqDTO.java
@@ -1,12 +1,15 @@
 package com.example.ExpenseTracker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record UpdateAccountReqDTO(
         @Size(min = 6, max = 20,  message = "{update.username.size}")
+        @Schema(description = "Username for login", example = "TestDev")
         String username,
 
         @Email(message = "{update.email.format}")
+        @Schema(description = "Email for login", example = "test@gmail.com")
         String email
 ) {}
